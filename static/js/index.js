bbd4a69eef76753af58a67b5e543000ccec82101
dmeaaxd <dmeaaxd@gmail.com>
2023-11-07 12:57:23 +0300

    Initial commit

---
+++ static/js/index.js
@@ -0,0 +1,234 @@
+// Получение токена пользователя из URL
+// const urlParams = new URLSearchParams(window.location.search);
+// const token = urlParams.get('token');
+
+token = sessionStorage.getItem('token');
+
+// Проверка, есть ли токен в URL
+if (token) {
+    // Выполнение запроса GraphQL с использованием токена
+    fetch('http://127.0.0.1:8000/users/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}` // Используйте токен пользователя
+        },
+        body: JSON.stringify({
+            query: `
+                        query {
+                            me {
+                                id
+                                lastLogin
+                                username
+                                firstName
+                                lastName
+                                email
+                                isStaff
+                                isActive
+                                dateJoined
+                                modelSet {
+                                    id
+                                    name
+                                    startDate
+                                    finishDate
+                                    startAmount
+                                }
+                                pk
+                                archived
+                                verified
+                                secondaryEmail
+                            }
+                        }
+                    `
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Обработка и вывод данных пользователя
+            const user = data.data.me;
+            const userData = `
+                    <p>Имя пользователя: ${user.username}</p>
+                    <!--Другие данные пользователя-->
+                `;
+            document.getElementById('userData').innerHTML = userData;
+
+            // Обработка и вывод списка моделей
+            const modelList = user.modelSet;
+            const modelListElement = document.getElementById('modelList');
+            modelList.forEach(model => {
+                const modelBlock = document.createElement('div'); // Создаем блок для модели
+                modelBlock.innerHTML = `
+                        <p>${model.name}</p>
+                        <p>${model.startAmount}</p>
+                        <p>${model.startDate} - ${model.finishDate}</p>
+                    `;
+
+                const goToModelButton = document.createElement('button'); // Кнопка "Перейти в модель"
+                goToModelButton.textContent = 'Перейти в модель';
+                goToModelButton.addEventListener('click', function () {
+                    window.location.href = `./model/model.html?name=${model.name}&id=${model.id}`;
+                });
+
+
+                const editButton = document.createElement('button'); // Кнопка "Редактировать"
+                editButton.textContent = 'Редактировать';
+                // Кнопка "Редактировать"
+                editButton.addEventListener('click', function () {
+                    const editForm = modelBlock.querySelector('.edit-form');
+
+                    if (!editForm) {
+                        // Если формы ещё нет, создаем её
+                        const newEditForm = document.createElement('div');
+                        newEditForm.classList.add('edit-form');
+
+                        newEditForm.innerHTML = `
+                                <input id="editModelName" required type="text" placeholder="Название" value="${model.name}"><br>
+                                <input id="editModelStartAmount" required type="text" placeholder="Начальная сумма" value="${model.startAmount}"><br>
+                                <input id="editModelStartDate" required type="text" placeholder="Начальная дата" value="${model.startDate}"><br>
+                                <input id="editModelFinishDate" required type="text" placeholder="Дата завершения" value="${model.finishDate}"><br>
+                                <button id="saveModelChanges">Сохранить</button>
+                            `;
+
+                        // Добавление формы редактирования в блок модели
+                        modelBlock.appendChild(newEditForm);
+
+                        // Обработчик события для кнопки "Сохранить"
+                        const saveModelChangesButton = newEditForm.querySelector('#saveModelChanges');
+                        saveModelChangesButton.addEventListener('click', function () {
+                            const newName = newEditForm.querySelector('#editModelName').value;
+                            const newStartAmount = newEditForm.querySelector('#editModelStartAmount').value;
+                            const newStartDate = newEditForm.querySelector('#editModelStartDate').value;
+                            const newFinishDate = newEditForm.querySelector('#editModelFinishDate').value;
+
+                            // Выполнение запроса GraphQL для обновления модели
+                            fetch('http://127.0.0.1:8000/finance/models', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${token}`
+                                },
+                                body: JSON.stringify({
+                                    query: `
+                                            mutation {
+                                                updateModel(
+                                                    finishDate: "${newFinishDate}"
+                                                    modelId: "${model.id}"
+                                                    name: "${newName}"
+                                                    startAmount: ${newStartAmount}
+                                                    startDate: "${newStartDate}"
+                                                ) {
+                                                    model {
+                                                        id
+                                                        name
+                                                        startDate
+                                                        finishDate
+                                                        startAmount
+                                                    }
+                                                }
+                                            }
+                                        `
+                                })
+                            });
+                            location.reload()
+                        });
+                    }
+                });
+
+
+                const deleteButton = document.createElement('button'); // Кнопка "Удалить"
+                deleteButton.textContent = 'Удалить';
+                deleteButton.addEventListener('click', function () {
+
+                    const shouldDelete = window.confirm('Вы уверены, что хотите удалить модель?');
+
+                    if (shouldDelete) {
+
+                        fetch('http://127.0.0.1:8000/finance/models', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${token}`
+                            },
+                            body: JSON.stringify({
+                                query: `
+                                    mutation {
+                                      deleteModel(modelId: ${model.id}) {
+                                        allModels {
+                                          id
+                                          name
+                                          startDate
+                                          finishDate
+                                          startAmount
+                                        }
+                                      }
+                                    }
+                                    `
+                            })
+                        })
+                        location.reload();
+                    }
+                });
+
+                modelBlock.appendChild(goToModelButton);
+                modelBlock.appendChild(editButton);
+                modelBlock.appendChild(deleteButton);
+
+                modelListElement.appendChild(modelBlock);
+            });
+
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+
+    // Обработчик события для кнопки "Новая модель"
+    const createModelButton = document.getElementById('createModel');
+    createModelButton.addEventListener('click', function () {
+        const createModelForm = document.getElementById('createNewModel');
+        createModelForm.style.display = 'block'; // Отображаем форму
+    });
+
+    // Обработчик события для кнопки "Сохранить"
+    const createButton = document.getElementById('createButton');
+    createButton.addEventListener('click', function () {
+        const newModelName = document.getElementById('newModelName').value;
+        const newModelAmount = document.getElementById('newModelAmount').value;
+        const newModelStartDate = document.getElementById('newModelStartDate').value;
+        const newModelFinishDate = document.getElementById('newModelFinishDate').value;
+
+        // Выполнение запроса GraphQL для создания модели
+        fetch('http://127.0.0.1:8000/finance/models', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation {
+                        createModel(
+                            finishDate: "${newModelFinishDate}"
+                            name: "${newModelName}"
+                            startAmount: ${newModelAmount}
+                            startDate: "${newModelStartDate}"
+                        ) {
+                            model {
+                                id
+                                name
+                                startDate
+                                finishDate
+                                startAmount
+                            }
+                        }
+                    }
+                `
+            })
+        })
+        location.reload()
+    });
+
+
+} else {
+    document.getElementById('userData').textContent = 'Токен пользователя не найден в URL.';
+}
---
+++ static/js/model/fill_table.js
@@ -0,0 +1,95 @@
+// Получение name и id модели из URL
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name');
+const id = urlParams.get('id');
+
+// Получение токена пользователя из URL
+// const token = urlParams.get('token');
+token = sessionStorage.getItem('token');
+
+
+if (name && id) {
+    fetch('http://127.0.0.1:8000/finance/dates', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            query: `
+                    query {
+                        modelDates(modelId: "${id}") {
+                            id
+                            date
+                            amount
+                            realAmount
+                            comment
+                            profits {
+                                name
+                                amount
+                            }
+                            expenses {
+                                name
+                                amount
+                            }
+                        }
+                    }
+                `
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            const modelData = data.data.modelDates;
+            const tableBody = document.getElementById('tableBody');
+
+            // sessionStorage.clear()
+
+            // Заполнение таблицы данными
+            modelData.forEach(item => {
+                const row = tableBody.insertRow();
+                row.id = `row_${item.id}`; // Устанавливаем ID строки
+
+
+
+                const cellDate = row.insertCell(0);
+                const cellModelAmount = row.insertCell(1); // Сумма
+                const cellFinancialOps = row.insertCell(2); // Финансовые операции
+                const cellRealityAmount = row.insertCell(3); // Сумма
+                const cellComment = row.insertCell(4); // Комментарий
+                const cellDifference = row.insertCell(5); // Разница
+
+
+                sessionStorage.setItem(item.date.toString(), item.id.toString())
+
+
+                // Вставляем данные в ячейки
+                cellDate.textContent = item.date;
+                cellModelAmount.textContent = item.amount;
+
+                // Финансовые операции (каждая операция в новой строке)
+                item.profits.forEach(profit => {
+                    const profitRow = document.createElement('div');
+                    profitRow.textContent = `${profit.name} +${profit.amount}`;
+                    cellFinancialOps.appendChild(profitRow);
+                });
+                item.expenses.forEach(expense => {
+                    const expenseRow = document.createElement('div');
+                    expenseRow.textContent = `${expense.name} -${expense.amount}`;
+                    cellFinancialOps.appendChild(expenseRow);
+                });
+
+                cellRealityAmount.textContent = item.realAmount;
+                cellComment.textContent = item.comment;
+
+                // Проверка наличия реальной суммы перед отображением разницы
+                if (item.realAmount !== null) {
+                    const difference = item.realAmount - item.amount;
+                    cellDifference.textContent = difference;
+                } else {
+                    const difference = "-";
+                    cellDifference.textContent = difference;
+                }
+
+            });
+        });
+}
